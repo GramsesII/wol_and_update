19a243d29c596f71dc77c02498eb80c513aa16af
Gramse <gramse@pln.nu>
2022-01-18 17:09:25 +0100

    further testing and coding done

--- autoconfig.sh
+++ autoconfig.sh
@@ -2,13 +2,17 @@
 GREEN='\e[0;32m'
 RED='\e[0;31m'
 RESET='\e[0m'
-start:
-        for name in .1st wol_config.cfg
+config="config_file.cfg"
+FILE=".1st"
+FILE2="config_file.cfg"
+input(){
+        for name in .1st config_file.cfg
         do
                 [ -f $name ] || { echo -en "${RED}First start 'auto-config creator'${RESET}\n";deps=1; }
         done
-                [[ $deps -ne 1 ]] && echo -ne "opa opa!" || {
-                        echo -en "\nTarget Broadcast IP (i.e 192.168.0.255, get by running 'fconfig' on the target machine).\n";
+#               [[ $deps -ne 1 ]] && { echo -ne "opa opa!\n"; exit 1; } || {
+               [[ $deps -ne 1 ]] && { [[ -f $FILE2 ]] &&  mv "$FILE2" "$FILE2-old"; } || {
+	                echo -en "\nTarget Broadcast IP (i.e 192.168.0.255, get by running 'fconfig' on the target machine).\n";
 			read -p ": " BROADCAST;
 			echo -en "\nTarget IP4 Macadress (looks like aa:bb:cc:dd:ee:ff, get by running 'ifconfig' on the target machine).\n";
 			read -p ": " MAC;
@@ -28,63 +32,42 @@ start:
                         echo -en "\nHow many seconds do we wait for the target machine to reboot\n";
 			echo -en "Raise or lower this if needed (default SEC=60).\n";
 			read -p ": " SEC; }
+	}
 
-config="config_file.cfg"
-#echo -ne "
+review(){
+	echo -en "
 # Start of auto configured '$config'
 # More info about this config in 'wol_config_example.cfg'
 
-#BROADCAST=$BROADCAST\n
-#MAC=$MAC\n
-#TARGET=$TARGET\n
-#PORT=$PORT\n
-#IFNAME=$IFNAME\n
-#USER=$USER\n
-#RSA=$RSA\n
-#SEC=$SEC\n
+BROADCAST=$BROADCAST\n
+MAC=$MAC\n
+TARGET=$TARGET\n
+PORT=$PORT\n
+IFNAME=$IFNAME\n
+USER=$USER\n
+RSA=$RSA\n
+SEC=$SEC\n
 # End of auto configured '$config'
-#" >> $config
+	"
+	}
 
-echo -en "this file only lets the script (WoL.sh) know if it is the first start or not, please ignore" > .1st
-#clear
-#echo -en "your $config looks like this"
-#cat $config
+input
+	clear
+	echo -en "This will be your config file.\n"
+review
+	echo -en "this file only lets the script (WoL.sh) know if it is the first start or not, please ignore" > .1st
+
 while true; do
-            echo -en "This will be your config file want to keep it.\n" && 
-		{ cat "
-# Start of auto configured '$config'
-# More info about this config in 'wol_config_example.cfg'
+	read -p "Wanna keep it ^_^ [y/n]? " yn
+	case $yn in
+	[Yy]* ) review >> $config; break;;
+	[Nn]* ) input;;
+	    * ) echo "Please answer yes or no.";;
+	esac
+ done
 
-BROADCAST=$BROADCAST\n
-MAC=$MAC\n
-TARGET=$TARGET\n
-PORT=$PORT\n
-IFNAME=$IFNAME\n
-USER=$USER\n
-RSA=$RSA\n
-SEC=$SEC\n
-# End of auto configured '$config'
-" }
-            read -p "[y/n]? " yn
-            case $yn in
-            [Yy]* ) { echo -en "
-# Start of auto configured '$config'
-# More info about this config in 'wol_config_example.cfg'
-
-BROADCAST=$BROADCAST\n
-MAC=$MAC\n
-TARGET=$TARGET\n
-PORT=$PORT\n
-IFNAME=$IFNAME\n
-USER=$USER\n
-RSA=$RSA\n
-SEC=$SEC\n
-# End of auto configured '$config'
-" >> $config; break;; }
-            [Nn]* ) jumpto $start;;
-            * ) echo "Please answer yes or no.";;
-            esac
-        done
-
+config_done(){
+echo -en "config done"
+}
 exit 0
 #EoF
