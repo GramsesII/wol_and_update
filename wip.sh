0888c365b4d954cff3ff4982023965cb6a10b1bd
Gramse <gramse@pln.nu>
2022-01-20 11:54:48 +0100

    development tree?

--- wip.sh
+++ wip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#  develop branch
 #  License: "(MIT)", (see LICENSE.txt for more info)
 #  Author: "Christian Berg <gramse@pln.nu> (https://pln.nu/)"
 #  Contributor(s): "That there decent man over there"
