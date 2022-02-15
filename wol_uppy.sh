26c2ff23e8b3159b769c54b486646cbe3b5b00a0
Gramse <gramse@pln.nu>
2022-02-15 20:22:30 +0100

    uppy time and added a fancy TODO.txt ;)

---
+++ wol_uppy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+APTCHECK="/usr/lib/update-notifier/apt-check"
+
+check_data=$($APTCHECK 2>&1)
+regex="^([0-9]+);([0-9]+)$"
+
+if [[ "$check_data" =~ $regex ]]
+then
+  if [[ ${BASH_REMATCH[1]} -ne 0 ]] || [[ ${BASH_REMATCH[2]} -ne 0 ]]
+  then
+  	echo -en "\nupdate\n" # line to be removed once done
+  	uptest=yes
+  	echo -en "$uptest\n" > .wol_answer
+    exit 0
+  fi
+fi
+echo -en "\nnothing\n" # line to be removed once done
+uptest=no
+echo -en "$uptest\n" > .wol_answer
+exit 1
+
+#EoF
